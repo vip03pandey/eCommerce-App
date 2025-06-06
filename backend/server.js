88db9b687cd3132f638c4a296b1ee73784a6e3e6
vipul <vipul03pandey@gmail.com>
2025-06-06 12:35:18 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,7 @@ const adminOrderRoutes=require('./routes/adminOrderRoutes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: [
-        'https://e-commerce-app-5avv.vercel.app', 
-        'http://localhost:3000',
-        'http://localhost:5173'
-      ],
-      
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-  
+app.use(cors())
 dotenv.config()
 connectDB ()
 
