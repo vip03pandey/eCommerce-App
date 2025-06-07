c9d8acce5b7f0216dff61fb99b4abab2e031fb69
vipul <vipul03pandey@gmail.com>
2025-06-07 10:18:09 +0530

    corsissue

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,15 @@ const adminOrderRoutes=require('./routes/adminOrderRoutes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+const corsOptions = {
+    origin: 'https://e-commerce-app-5avv.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  };
+  
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions))
 dotenv.config()
 connectDB ()
 
